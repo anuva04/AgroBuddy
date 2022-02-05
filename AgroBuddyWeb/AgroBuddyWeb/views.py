1a31a5cb5364773d5d77547bd045a4fd9cc4333f
anuva04 <57056078+anuva04@users.noreply.github.com>
2022-02-05 17:41:49 +0530

    commit

--- AgroBuddyWeb/AgroBuddyWeb/views.py
+++ AgroBuddyWeb/AgroBuddyWeb/views.py
@@ -1,6 +1,31 @@
+from ast import parse
 from django.shortcuts import render
 import pickle
 import os
+import wikipedia
+import requests
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+FLICKR_API_KEY = env('FLICKR_API_KEY')
+
+# method to get wikipedia snippet
+def wiki_snippet(search):
+    try:
+        result = wikipedia.summary(search, sentences = 3)
+    except:
+        return "Wrong Input"
+    return result
+
+# method to get image of a specific keyword
+def get_image_url(search):
+    search_by_key_url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key='+FLICKR_API_KEY+'&tags='+search+'&format=json&nojsoncallback=1'
+    response = requests.get(search_by_key_url).json()
+    res = response['photos']['photo'][0]
+    current_photo_url = 'https://farm'+str(res['farm'])+'.staticflickr.com/'+str(res['server'])+'/'+str(res['id'])+'_'+str(res['secret'])+'_n.jpg'
+    return current_photo_url
 
 #homepage view
 def home(request):
@@ -12,7 +37,11 @@ def getPredictions(ph, moisture, humidity, temperature):
     sc = pickle.load(open(os.path.dirname(os.path.realpath(__file__)) + '\scaler.sav', "rb"))
     prediction = model.predict(sc.transform([[ph, moisture, humidity, temperature]]))
 
-    return prediction
+    best_prediction = prediction[0]
+    wikipedia_snippet = wiki_snippet(best_prediction)
+    image_url = get_image_url(best_prediction)
+
+    return [best_prediction.upper(), wikipedia_snippet, image_url]
 
 # method for rendering result
 def result(request):
@@ -23,5 +52,5 @@ def result(request):
 
     result = getPredictions(ph, moisture, humidity, temperature)
 
-    return render(request, 'result.html', {'result':result})
+    return render(request, 'result.html', {'ph':ph, 'moisture':moisture, 'humidity':humidity, 'temp':temperature, 'result':result[0], 'wiki':result[1], 'image':result[2]})
     
