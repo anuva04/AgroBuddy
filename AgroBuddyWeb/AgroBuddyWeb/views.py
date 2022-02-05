dba81916eae205d84fef7bc1b653738560b5959d
anuva04 <57056078+anuva04@users.noreply.github.com>
2022-02-05 18:50:05 +0530

    Bug fixes

--- AgroBuddyWeb/AgroBuddyWeb/views.py
+++ AgroBuddyWeb/AgroBuddyWeb/views.py
@@ -6,25 +6,36 @@ import wikipedia
 import requests
 import environ
 
+# retrieving environment variables
 env = environ.Env()
 environ.Env.read_env()
-
 FLICKR_API_KEY = env('FLICKR_API_KEY')
+PIXABAY_API_KEY = env('PIXABAY_API_KEY')
 
 # method to get wikipedia snippet
 def wiki_snippet(search):
     try:
         result = wikipedia.summary(search, sentences = 3)
     except:
-        return "Wrong Input"
+        try:
+            result = wikipedia.summary(search, sentences = 3)
+        except:
+            return "Please Click"
     return result
 
-# method to get image of a specific keyword
-def get_image_url(search):
+# method to get image of a specific keyword using Flickr API
+def get_image_url_flickr(search):
     search_by_key_url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key='+FLICKR_API_KEY+'&tags='+search+'&format=json&nojsoncallback=1'
     response = requests.get(search_by_key_url).json()
     res = response['photos']['photo'][0]
     current_photo_url = 'https://farm'+str(res['farm'])+'.staticflickr.com/'+str(res['server'])+'/'+str(res['id'])+'_'+str(res['secret'])+'_n.jpg'
+    return current_photo_url
+
+# method to get image of a specific keyword using pixabay API
+def get_image_url_pixabay(search):
+    search_by_key_url = 'https://pixabay.com/api/?key='+PIXABAY_API_KEY+'&q='+search+'&image_type=photo'
+    response = requests.get(search_by_key_url).json()
+    current_photo_url = response['hits'][1]['webformatURL']
     return current_photo_url
 
 #homepage view
@@ -39,7 +50,7 @@ def getPredictions(ph, moisture, humidity, temperature):
 
     best_prediction = prediction[0]
     wikipedia_snippet = wiki_snippet(best_prediction)
-    image_url = get_image_url(best_prediction)
+    image_url = get_image_url_pixabay(best_prediction)
 
     return [best_prediction.upper(), wikipedia_snippet, image_url]
 
